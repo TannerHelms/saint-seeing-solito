0be3de0e09bf1b7c13231c5c3b99e5ca9015fdbd
TannerHelms <tannerhelms01@gmail.com>
2024-05-12 19:14:26 -0600

    added toast

--- apps/expo/app/_layout.tsx
+++ apps/expo/app/_layout.tsx
@@ -1,30 +1,31 @@
-import 'react-native-gesture-handler';
-import { Ionicons } from '@expo/vector-icons';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { auth } from 'app/features/auth';
-import { HomeScreen } from 'app/features/home/screen';
-import { MeScreen } from 'app/features/me/screen';
-import DetailsScreen from 'app/features/messages/details';
-import MessageScreen from 'app/features/messages/screen';
-import ProfileScreen from 'app/features/profile/screen';
-import PasswordScreen from 'app/features/profile/password';
-import SignOutScreen from 'app/features/profile/sign-out';
-import { Provider } from 'app/provider';
-import { Stack } from 'expo-router';
-import { onAuthStateChanged, signOut } from 'firebase/auth';
-import { useEffect, useState } from 'react';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import 'react-native-gesture-handler'
+import { Ionicons } from '@expo/vector-icons'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { auth } from 'app/features/auth'
+import { HomeScreen } from 'app/features/home/screen'
+import { MeScreen } from 'app/features/me/screen'
+import DetailsScreen from 'app/features/messages/details'
+import MessageScreen from 'app/features/messages/screen'
+import ProfileScreen from 'app/features/profile/screen'
+import PasswordScreen from 'app/features/profile/password'
+import SignOutScreen from 'app/features/profile/sign-out'
+import { Provider } from 'app/provider'
+import { Stack } from 'expo-router'
+import { onAuthStateChanged, signOut } from 'firebase/auth'
+import { useEffect, useState } from 'react'
+import { createDrawerNavigator } from '@react-navigation/drawer'
+import Toast from 'react-native-toast-message'
 
 const Tab = createBottomTabNavigator()
-const HomeStack = createNativeStackNavigator();
-const MessageStack = createNativeStackNavigator();
-const ProfileDrawer = createDrawerNavigator();
+const HomeStack = createNativeStackNavigator()
+const MessageStack = createNativeStackNavigator()
+const ProfileDrawer = createDrawerNavigator()
 
 export default function Root() {
   const [user, setUser] = useState(auth.currentUser)
-  
+
   useEffect(() => {
     const event = onAuthStateChanged(auth, (user) => {
       setUser(user)
@@ -32,80 +33,82 @@ export default function Root() {
     return () => event()
   }, [])
 
-  if (!user || user == undefined) return (
-    <Provider>
-      <Stack>
-        <Stack.Screen name="index" options={
-          { title: 'Home' ,
-            headerShown: false
-          }
-        } />
-      </Stack>
-    </Provider>
-  )
+  if (!user || user == undefined)
+    return (
+      <Provider>
+        <Stack>
+          <Stack.Screen
+            name="index"
+            options={{ title: 'Home', headerShown: false }}
+          />
+        </Stack>
+      </Provider>
+    )
   return (
-    <NavigationContainer independent={true}>
-      <Tab.Navigator screenOptions={{ headerShown: false }}>
-        <Tab.Screen 
-        name="index" 
-        component={Home}  
-        options={{
-          tabBarLabel: 'Home',
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="home" color={color} size={size} />
-          ),
-        }}
-        />
-        <Tab.Screen 
-        name="messages" 
-        component={Messages}  
-        options={{
-          tabBarLabel: 'Messages',
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="chatbox" color={color} size={size} />
-          ),
-        }}
-        />
-        <Tab.Screen 
-        name="me" 
-        component={Profile}  
-        options={{
-          tabBarLabel: 'Profile',
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="person" color={color} size={size} />
-          ),
-        }}
-        />        
-      </Tab.Navigator>
-  </NavigationContainer>
+    <>
+      <NavigationContainer independent={true}>
+        <Tab.Navigator screenOptions={{ headerShown: false }}>
+          <Tab.Screen
+            name="index"
+            component={Home}
+            options={{
+              tabBarLabel: 'Home',
+              tabBarIcon: ({ color, size }) => (
+                <Ionicons name="home" color={color} size={size} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="messages"
+            component={Messages}
+            options={{
+              tabBarLabel: 'Messages',
+              tabBarIcon: ({ color, size }) => (
+                <Ionicons name="chatbox" color={color} size={size} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="me"
+            component={Profile}
+            options={{
+              tabBarLabel: 'Profile',
+              tabBarIcon: ({ color, size }) => (
+                <Ionicons name="person" color={color} size={size} />
+              ),
+            }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+      <Toast />
+    </>
   )
 }
- 
 
 function Home() {
   return (
     <HomeStack.Navigator>
-      <HomeStack.Screen name="Home" component={HomeScreen}   />
+      <HomeStack.Screen name="Home" component={HomeScreen} />
       <HomeStack.Screen name="profile" component={ProfileScreen} />
-  </HomeStack.Navigator>
+    </HomeStack.Navigator>
   )
 }
 
 function Profile() {
   return (
     <ProfileDrawer.Navigator>
-      <ProfileDrawer.Screen name="Profile" component={MeScreen}   />
-      <ProfileDrawer.Screen name="Edit Password" component={PasswordScreen}   />
-      <ProfileDrawer.Screen name="Sign Out" component={SignOutScreen}   />
-  </ProfileDrawer.Navigator>
+      <ProfileDrawer.Screen name="Profile" component={MeScreen} />
+      <ProfileDrawer.Screen name="Edit Password" component={PasswordScreen} />
+      <ProfileDrawer.Screen name="Sign Out" component={SignOutScreen} />
+    </ProfileDrawer.Navigator>
   )
 }
 
 function Messages() {
   return (
     <MessageStack.Navigator>
-      <MessageStack.Screen name="Messages" component={MessageScreen}   />
-      <MessageStack.Screen name="details" component={DetailsScreen}   />
-  </MessageStack.Navigator>
+      <MessageStack.Screen name="Messages" component={MessageScreen} />
+      <MessageStack.Screen name="details" component={DetailsScreen} />
+    </MessageStack.Navigator>
   )
 }
--- packages/app/features/profile/password.tsx
+++ packages/app/features/profile/password.tsx
@@ -1,40 +1,68 @@
-import { View } from "packages/app/design/view";
-import { useEffect, useState } from "react";
-import CustomButton from "../utils/custom-button";
-import CustomTextInput from "../utils/custom-text-input";
-import { updatePassword } from "firebase/auth";
-import { auth } from "../auth";
-import { useRouter } from "solito/router";
-export default function PasswordScreen({navigation}) {
-    const router = useRouter()
-    const [password, setPassword] = useState<string>('')
+import { View } from 'packages/app/design/view'
+import { useEffect, useState } from 'react'
+import CustomButton from '../utils/custom-button'
+import CustomTextInput from '../utils/custom-text-input'
+import { updatePassword } from 'firebase/auth'
+import { auth } from '../auth'
+import { useRouter } from 'solito/router'
+import Toast from 'react-native-toast-message'
+export default function PasswordScreen({ navigation }) {
+  const router = useRouter()
+  const [password, setPassword] = useState<string>('')
 
-    useEffect(() => {
-        navigation.getParent()?.setOptions({
-          tabBarStyle: {
-            display: "none"
-          }
-        });
-        return () => navigation.getParent()?.setOptions({
-          tabBarStyle: undefined
-        });
-      }, [navigation]);
-
-    
-    async function handleSubmit() {
-        updatePassword(auth.currentUser!!, password).then(() => {
-            setPassword('')
-            router.replace('/Profile')
-        }).catch((error) => {
-            console.log(error)
-        })
+  const showToast = (success = true) => {
+    if (success) {
+      Toast.show({
+        type: 'success',
+        text1: 'Password Updated',
+        text2: 'Your password has been updated successfully',
+      })
+    } else {
+      Toast.show({
+        type: 'error',
+        text1: 'Error!',
+        text2: 'There was an error updating your password. Please try again.',
+      })
     }
+  }
 
-    return (
-        <View className="px-2 space-y-4 pt-4">
-            <CustomTextInput onChange={setPassword} icon="lock-closed" placeholder="New Password..."/>
-            <CustomButton text="Change Password" onPress={handleSubmit} className="bg-primary rounded-xl py-2" textColor="text-black" />
-        </View>
-    )
+  useEffect(() => {
+    navigation.getParent()?.setOptions({
+      tabBarStyle: {
+        display: 'none',
+      },
+    })
+    return () =>
+      navigation.getParent()?.setOptions({
+        tabBarStyle: undefined,
+      })
+  }, [navigation])
+
+  async function handleSubmit() {
+    updatePassword(auth.currentUser!!, password)
+      .then(() => {
+        showToast()
+        setPassword('')
+        router.replace('/Profile')
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
+  return (
+    <View className="space-y-4 px-2 pt-4">
+      <CustomTextInput
+        onChange={setPassword}
+        icon="lock-closed"
+        placeholder="New Password..."
+      />
+      <CustomButton
+        text="Change Password"
+        onPress={handleSubmit}
+        className="bg-primary rounded-xl py-2"
+        textColor="text-black"
+      />
+    </View>
+  )
 }
-
